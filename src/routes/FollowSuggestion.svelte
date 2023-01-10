76ec6fb31637cc4b9e024474862217207561e44c
Matt Lehrer <lehrerm@gmail.com>
2023-01-10 21:55:51 +0100

    move up breakpoint for long names

--- src/routes/FollowSuggestion.svelte
+++ src/routes/FollowSuggestion.svelte
@@ -7,20 +7,20 @@
 </script>
 
 <article class="block max-w-3xl overflow-hidden hover:bg-brand-100 sm:mx-8 md:mx-16">
-	<div class="flex flex-col items-start p-4 sm:mx-0 sm:flex-row sm:p-6">
-		<div class="flex w-full items-center justify-between sm:items-start">
+	<div class="flex flex-col items-start p-4 md:mx-0 md:flex-row md:p-6">
+		<div class="flex w-full items-center justify-between md:items-start">
 			<div class="flex-shrink-0">
 				<img
-					class="h-16 w-16 rounded-lg bg-white shadow sm:h-24 sm:w-24"
+					class="h-16 w-16 rounded-lg bg-white shadow md:h-24 md:w-24"
 					loading="lazy"
 					src={account.avatar_static}
 					alt={account.display_name}
 				/>
 			</div>
-			<div class="ml-4 hidden min-w-0 flex-1 sm:ml-6 sm:flex sm:items-center sm:justify-between">
+			<div class="ml-4 hidden min-w-0 flex-1 md:ml-6 md:flex md:items-center md:justify-between">
 				<AccountDetails {account} />
 			</div>
-			<div class="flex-shrink-0 sm:ml-5">
+			<div class="flex-shrink-0 md:ml-5">
 				<a
 					href={`https://${host}/authorize_follow?acct=${account.acct}`}
 					target="_blank"
@@ -33,7 +33,7 @@
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 20 20"
 						fill="currentColor"
-						class="ml-3 h-5 w-5 text-accent-100 sm:-mr-1"
+						class="ml-3 h-5 w-5 text-accent-100 md:-mr-1"
 					>
 						<path
 							d="M11 5a3 3 0 11-6 0 3 3 0 016 0zM2.615 16.428a1.224 1.224 0 01-.569-1.175 6.002 6.002 0 0111.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 018 18a9.953 9.953 0 01-5.385-1.572zM16.25 5.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z"
@@ -43,7 +43,7 @@
 			</div>
 		</div>
 
-		<div class="mt-3 min-w-0 flex-1 sm:hidden">
+		<div class="mt-3 min-w-0 flex-1 md:hidden">
 			<AccountDetails {account} />
 		</div>
 	</div>
