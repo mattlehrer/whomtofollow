42a6dfddd52cc906cada287eb3ae99d019fa449c
Matt Lehrer <lehrerm@gmail.com>
2023-01-13 15:39:51 +0100

    wip

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected input label', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+	expect(await page.textContent('label')).toBe('Your Fediverse Account:');
 });
