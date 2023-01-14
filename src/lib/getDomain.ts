6c1db36e8b5d618f519152976b6d0bced7b225f8
Matt Lehrer <lehrerm@gmail.com>
2023-01-14 12:56:46 +0100

    refactor getDomain

---
+++ src/lib/getDomain.ts
@@ -0,0 +1,68 @@
+import { get } from 'svelte/store';
+import { accountData, hosts } from './data';
+import { Timeout } from './utils/timeout';
+
+export async function getDomain(acct: string): Promise<string> {
+	const server = acct.split('@')[1];
+	if (get(hosts).has(server)) {
+		// assume that all other users with the same domain are on the same server
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		return get(hosts).get(server)!;
+	}
+
+	if (get(accountData).has(acct)) {
+		const account = get(accountData).get(acct);
+		if (account?.url) {
+			const match = account.url.match(/^https?:\/\/([\w-]+(\.[\w-]+)+)/);
+			if (match?.length === 3) {
+				return match[1];
+			}
+		}
+	}
+
+	// check webfinger
+	const match = acct.match(/^@?(.+)@(.+)$/);
+	if (match?.length !== 3) {
+		throw new Error(`Incorrect handle: ${acct}`);
+	}
+	const domain = match[2];
+
+	try {
+		const webfingerResp = await fetch(
+			`https://${domain}/.well-known/webfinger?resource=acct:${acct}`,
+			{
+				signal: Timeout(2000).signal,
+			},
+		);
+		const webfinger = await webfingerResp.json();
+		const links = webfinger.links;
+		type AcctLink = {
+			rel: string;
+			type: string;
+			href: string;
+		};
+		const acctLink: AcctLink = links.find(
+			(l: AcctLink) => l.rel === 'self' && l.type === 'application/activity+json',
+		);
+		if (!acctLink) {
+			throw new Error(`No activity pub link for ${acct}`);
+		}
+		const acctUrl = new URL(acctLink.href);
+		get(hosts).set(server, acctUrl.host);
+		return acctUrl.host;
+	} catch (error) {
+		// console.log('getDomain', error, acct);
+	}
+	try {
+		const webfingerResp = await fetch(`/api/webfinger/${domain}/${acct}`, {
+			signal: Timeout(2000).signal,
+		});
+		const acctLinkHref = await webfingerResp.text();
+		const acctUrl = new URL(acctLinkHref);
+		get(hosts).set(server, acctUrl.host);
+		return acctUrl.host;
+	} catch (error) {
+		console.debug('getDomain webfinger', error, acct);
+	}
+	throw new Error(`Error getting domain for ${acct}`);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,13 @@
 	import { fade } from 'svelte/transition';
 	import type { Account } from '../lib/Account';
 	import Errors from './Errors.svelte';
-	import { accountData, hosts } from '../lib/data';
+	import { accountData } from '../lib/data';
 	import FollowSuggestion from './FollowSuggestion.svelte';
 	import type { PageData } from './$types';
 	import Footer from '$lib/Footer.svelte';
 	import { Timeout } from '$lib/utils/timeout';
 	import { fulfilledValues } from '$lib/utils/promises';
+	import { getDomain } from '$lib/getDomain';
 
 	export let data: PageData;
 
@@ -47,71 +48,6 @@
 	}
 
 	const AccountRegex = /^@?[\w-]+@[\w-]+(\.[\w-]+)+$/;
-
-	async function getDomain(acct: string): Promise<string> {
-		const server = acct.split('@')[1];
-		if ($hosts.has(server)) {
-			// assume that all other users with the same domain are on the same server
-			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			return $hosts.get(server)!;
-		}
-
-		if ($accountData.has(acct)) {
-			const account = $accountData.get(acct);
-			if (account?.url) {
-				const match = account.url.match(/^https?:\/\/([\w-]+(\.[\w-]+)+)/);
-				if (match?.length === 3) {
-					return match[1];
-				}
-			}
-		}
-
-		// check webfinger
-		const match = acct.match(/^@?(.+)@(.+)$/);
-		if (match?.length !== 3) {
-			throw new Error(`Incorrect handle: ${acct}`);
-		}
-		const domain = match[2];
-
-		try {
-			const webfingerResp = await fetch(
-				`https://${domain}/.well-known/webfinger?resource=acct:${acct}`,
-				{
-					signal: Timeout(2000).signal,
-				},
-			);
-			const webfinger = await webfingerResp.json();
-			const links = webfinger.links;
-			type AcctLink = {
-				rel: string;
-				type: string;
-				href: string;
-			};
-			const acctLink: AcctLink = links.find(
-				(l: AcctLink) => l.rel === 'self' && l.type === 'application/activity+json',
-			);
-			if (!acctLink) {
-				throw new Error(`No activity pub link for ${acct}`);
-			}
-			const acctUrl = new URL(acctLink.href);
-			$hosts.set(server, acctUrl.host);
-			return acctUrl.host;
-		} catch (error) {
-			// console.log('getDomain', error, acct);
-		}
-		try {
-			const webfingerResp = await fetch(`/api/webfinger/${domain}/${acct}`, {
-				signal: Timeout(2000).signal,
-			});
-			const acctLinkHref = await webfingerResp.text();
-			const acctUrl = new URL(acctLinkHref);
-			$hosts.set(server, acctUrl.host);
-			return acctUrl.host;
-		} catch (error) {
-			console.debug('getDomain webfinger', error, acct);
-		}
-		throw new Error(`Error getting domain for ${acct}`);
-	}
 
 	async function getAccountInfo(acct: Account['acct'], force = false): Promise<Account> {
 		if (!force && $accountData.has(acct)) {
