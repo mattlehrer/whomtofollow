6abeb142a6a184c8b37fedf93b3358a2625fdb1c
Matt Lehrer <lehrerm@gmail.com>
2023-01-14 11:41:17 +0100

    refactor form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import SearchForm from './SearchForm.svelte';
+
 	import VirtualScroll from 'svelte-virtual-scroll-list';
 	import { fade } from 'svelte/transition';
 	import type { Account } from './Account';
@@ -41,13 +43,6 @@
 				(a) => a.followed_by.size / a.followers_count <= 1,
 			);
 		}
-		// if (!isLoading) {
-		// 	console.log('accountsYouMightFollow[0]', JSON.stringify(accountsYouMightFollow[0], null, 2));
-		// 	console.log(
-		// 		'followed_by',
-		// 		JSON.stringify([...accountsYouMightFollow[0].followed_by], null, 2),
-		// 	);
-		// }
 	}
 
 	const AccountRegex = /^@?[\w-]+@[\w-]+(\.[\w-]+)+$/;
@@ -356,71 +351,19 @@
 	<meta name="description" content="Find people to follow on the Fediverse" />
 </svelte:head>
 
-<main>
+<main class="pt-4">
 	{#if !accountsYouMightFollow.length}
 		<div class="flex flex-col justify-between h-full">
 			<div class="p-4 sm:p-8 md:p-16">
-				<form class="max-w-2xl sm:px-4" on:submit={search}>
-					<label
-						for="account"
-						class="ml-px block pl-4 text-3xl font-medium text-brand-700 sm:text-4xl"
-						>Your Fediverse Account:</label
-					>
-					<div class="mt-3">
-						<input
-							type="text"
-							name="account"
-							id="account"
-							required
-							pattern={AccountRegex.source}
-							title="Please enter a valid account including the username, the @ symbol, and the host domain."
-							bind:value={account}
-							class="block w-full rounded-full border-slate-700 px-4 shadow-sm focus:border-brand-500 focus:ring-brand-500"
-							placeholder="gargron@mastodon.social"
-						/>
-					</div>
-					<button
-						on:click|preventDefault={search}
-						disabled={isLoading}
-						type="button"
-						class="mt-6 inline-flex  items-center rounded-full border border-transparent bg-brand-600 px-4 py-2 text-lg font-medium text-brand-100 shadow-sm hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 disabled:opacity-25"
-						>Find people you may know</button
-					>
-				</form>
+				<SearchForm bind:account bind:isLoading on:submit={search} />
 			</div>
 			<Footer />
 		</div>
 	{/if}
 	{#if accountsYouMightFollow.length}
 		<VirtualScroll data={accountsYouMightFollow} key="id" let:data>
-			<div class="mt-4 p-4 pb-8 sm:p-8 md:p-16" slot="header">
-				<form class="max-w-2xl sm:px-4" on:submit={search}>
-					<label
-						for="account"
-						class="ml-px block pl-4 text-3xl font-medium text-brand-700 sm:text-4xl"
-						>Your Fediverse Account:</label
-					>
-					<div class="mt-3">
-						<input
-							type="text"
-							name="account"
-							id="account"
-							required
-							pattern={AccountRegex.source}
-							title="Please enter a valid account including the username, the @ symbol, and the host domain."
-							bind:value={account}
-							class="block w-full rounded-full border-slate-700 px-4 shadow-sm focus:border-brand-500 focus:ring-brand-500"
-							placeholder="gargron@mastodon.social"
-						/>
-					</div>
-					<button
-						on:click|preventDefault={search}
-						disabled={isLoading}
-						type="button"
-						class="mt-6 inline-flex  items-center rounded-full border border-transparent bg-brand-600 px-4 py-2 text-lg font-medium text-brand-100 shadow-sm hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 disabled:opacity-25"
-						>Find people you may know</button
-					>
-				</form>
+			<div class="p-4 pb-8 sm:p-8 md:p-16" slot="header">
+				<SearchForm bind:account bind:isLoading on:submit={search} />
 			</div>
 
 			<FollowSuggestion account={data} {host} />
---
+++ src/routes/SearchForm.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let account: string;
+	export let isLoading: boolean;
+
+	const AccountRegex = /^@?[\w-]+@[\w-]+(\.[\w-]+)+$/;
+</script>
+
+<form class="max-w-2xl sm:px-4" on:submit>
+	<label for="account" class="ml-px block pl-4 text-3xl font-medium text-brand-700 sm:text-4xl"
+		>Your Fediverse Account:</label
+	>
+	<div class="mt-3">
+		<input
+			type="text"
+			name="account"
+			id="account"
+			required
+			pattern={AccountRegex.source}
+			title="Please enter a valid account including the username, the @ symbol, and the host domain."
+			bind:value={account}
+			class="block w-full rounded-full border-slate-700 px-4 shadow-sm focus:border-brand-500 focus:ring-brand-500"
+			placeholder="gargron@mastodon.social"
+		/>
+	</div>
+	<button
+		disabled={isLoading}
+		type="submit"
+		class="mt-6 inline-flex  items-center rounded-full border border-transparent bg-brand-600 px-4 py-2 text-lg font-medium text-brand-100 shadow-sm hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 disabled:opacity-25"
+		>Find people you may know</button
+	>
+</form>
